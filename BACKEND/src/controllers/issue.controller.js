fc569c934b4e4f61b78a12dc6ab40dcbf4054471
HariHara-sn <sudhanabirami007@gmail.com>
2026-01-14 16:29:26 +0530

    Issue Assigned Module Completed

--- BACKEND/src/controllers/issue.controller.js
+++ BACKEND/src/controllers/issue.controller.js
@@ -1,5 +1,7 @@
 //Controller for Student Submitting an Issue
+import logger from "../config/logger.js";
 import Issue from "../models/issue.model.js";
+import User from "../models/user.model.js";
 
 export const createIssue = async (req, res) => {
   try {
@@ -10,16 +12,18 @@ export const createIssue = async (req, res) => {
       description,
       location,
       priority,
-      createdBy: req.user.id,  // student ID from JWT
+      createdBy: req.user.id, // student ID from JWT
     });
 
     res.status(201).json({
       message: "Issue submitted successfully",
       issue,
     });
+    logger.info(`Issue Submitted successfully ✅`);
 
   } catch (error) {
     console.error(error);
+    logger.error(error);
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
@@ -27,12 +31,60 @@ export const createIssue = async (req, res) => {
 // Admin Should See “Issues Waiting for Assignment"
 export const getUnassignedIssues = async (req, res) => {
   try {
-    const issues = await Issue.find({ status: "Pending" }).populate("createdBy", "name email");
+    const issues = await Issue.find({ status: "Pending" }).populate(
+      "createdBy",
+      "name email"
+    );
 
     res.json({ issues });
-
   } catch (error) {
+    logger.error(error);
+
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
 
+export const stafflist = async (req, res) => {
+  try {
+    const staff = await User.find({ role: "staff" }).select("name email");
+    res.json({ staff });
+
+  } catch (err) {
+    logger.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const assignIssueToStaff = async (req, res) => {
+  try {
+    const { issueId, staffId } = req.body;
+
+    if (!issueId || !staffId) {
+      return res.status(400).json({ message: "Issue ID and Staff ID are required" });
+    }
+
+    const updatedIssue = await Issue.findByIdAndUpdate(
+      issueId,
+      {
+        assignedTo: staffId,
+        status: "Assigned",
+      },
+      { new: true }
+
+    ).populate("assignedTo", "name email role");
+
+    if (!updatedIssue) {
+      return res.status(404).json({ message: "Issue not found" });
+    }
+
+    res.json({
+      message: "Issue assigned successfully",
+      issue: updatedIssue,
+    });
+    logger.info(`Issue assigned successfully ✅`);
+
+  } catch (error) {
+    logger.error(error);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
--- BACKEND/src/routes/issue.routes.js
+++ BACKEND/src/routes/issue.routes.js
@@ -3,11 +3,20 @@ import { createIssue } from "../controllers/issue.controller.js";
 import { protect } from "../middlewares/auth.middleware.js";
 import { authorizeRoles } from "../middlewares/role.middleware.js";
 import { getUnassignedIssues } from "../controllers/issue.controller.js";
+import { assignIssueToStaff } from "../controllers/issue.controller.js";
+import { stafflist } from "../controllers/issue.controller.js";
+// import User from "../models/user.model.js";
 const router = express.Router();
 
 // ONLY STUDENT CAN CREATE ISSUE
 router.post("/create", protect, authorizeRoles("student"), createIssue);
 
 router.get("/pending", protect, authorizeRoles("admin"), getUnassignedIssues);
+
+router.put("/assign", protect, authorizeRoles("admin"), assignIssueToStaff);
+
+// Get all staff list
+router.get("/staff", protect, authorizeRoles("admin"), stafflist);
+
 
 export default router;
